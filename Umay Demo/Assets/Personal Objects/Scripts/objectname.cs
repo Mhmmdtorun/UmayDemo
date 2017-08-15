befd38199583fcf76244905003da7c529fa75fe3
Mhmmdtorun <nurullah-torun@outlook.com>
2017-08-15 22:00:41 +0300

    update

---
+++ Umay Demo/Assets/Personal Objects/Scripts/objectname.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class objectname : MonoBehaviour {
+	
+	RaycastHit hit;
+	public Text yazi;
+	void Start () {
+	
+	}
+	
+	
+	void Update () {
+		if (Physics.Raycast(Camera.main.transform.position,Camera.main.transform.forward,out hit,2))
+	{
+			if (hit.transform.gameObject.tag=="vida") {
+				Debug.Log (hit.transform.gameObject.tag);
+
+			}
+	}
+	}
+}
